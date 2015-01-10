632fc810a4e9b0fa7fba2c7d53111c16dcf89139
Yu Cao <cygeforce@gmail.com>
2015-01-10 22:15:52 +0000

    First commit for tdd project

---
+++ microsoft/visual_studio/SampleProgram/ChessLib/BishopMove.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChessLib
+{
+    public class BishopMove
+    {
+        public IEnumerable<Position> ValidMovesFor(Position pos)
+        {
+            ISet<Position> positions = new HashSet<Position>();
+            positions.Add(new Position(1, 2));
+            return positions;
+        }
+    }
+}
